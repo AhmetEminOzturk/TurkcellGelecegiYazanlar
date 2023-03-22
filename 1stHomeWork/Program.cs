ba3a63aa2e9cbd4e461b500b54103d29d49bec5e
AhmetEminOzturk <116569921+AhmetEminOzturk@users.noreply.github.com>
2023-03-22 16:19:18 +0300

    update

---
+++ 1stHomeWork/Program.cs
@@ -0,0 +1,44 @@
+﻿//diziye ait elemanların toplamlarını - ortalamalarını - en buyuk sayısını ve en kucuk sayısını veren programı dongu kullanarak yazınız
+
+
+Console.WriteLine("Enter 5 numbers");
+int[] numbers = new int[5];
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"Number {i + 1}: ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("******************************************");
+//dizi elemanlarının toplamı
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    sum += numbers[i];
+}
+Console.WriteLine($"Sum of numbers: {sum}");
+
+//dizi elemanlarının aritmetik ortalaması
+Console.WriteLine("Average of numbers: " + (sum / numbers.Length));
+
+//dizi elemanlarının en buyugu
+int maxNumber = int.MinValue;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] >= maxNumber)
+    {
+        maxNumber = numbers[i];
+    }
+}
+Console.WriteLine($"Max number: {maxNumber}");
+
+//dizi elemanlarının en kucugu
+int minNumber = int.MaxValue;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] <= minNumber)
+    {
+        minNumber = numbers[i];
+    }
+}
+Console.WriteLine($"Min number: {minNumber}");
---
+++ 2ndHomeWork/Program.cs
@@ -0,0 +1,28 @@
+﻿var password = string.Empty;
+while (password.Length < 6)
+{
+    Console.WriteLine("Enter a password with minimum 6 Digit");
+    password = Console.ReadLine();
+    if (password.Length >= 6)
+        break;
+    else
+        continue;
+}
+
+
+List<char> characters = new List<char>();
+foreach (var item in password)
+    characters.Add(item);
+
+
+int isNumber = 0;
+int isChar = 0;
+foreach (var item in characters)
+{
+    if (char.IsNumber(item))
+        isNumber++;
+    if (char.IsLetter(item))
+        isChar++;
+}
+
+Console.WriteLine(isNumber == characters.Count ? "Weak Password" : isChar == characters.Count ? "Weak Password" : (isChar + isNumber) == characters.Count ? "Avarage Password" : "Strong Password");
